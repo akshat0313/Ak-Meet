c99aec41e40a27b73718333a4b5b2ba31a041a06
Archisman Karmakar <92569441+ArchismanKarmakar@users.noreply.github.com>
2021-12-18 23:44:26 +0530

    Update scrsh.js

--- public/scrsh.js
+++ public/scrsh.js
@@ -1,25 +1,74 @@
+var Peer = window.SimplePeer;
 var socket1 = io.connect();
-// socket=io.connect();
 
-navigator.mediaDevices.getUserMedia({  
-     video: {  
-       mediaSource: "screen",  
-       width: { max: '640' },  
-       height: { max: '480' },   
-       frameRate: { max: '10' }  
-     }  
-   }).then(gotMedia);
-
-   function gotMedia (stream) {  
-     var video = document.querySelector('video');  
-     video.srcObject = stream;  
-     video.play();  
-   }
-
-var initiateBtn = document.getElementById('initiateBtn');  
+var initiateBtn = document.getElementById('initiateBtn');
+var stopBtn = document.getElementById('stopBtn');
 var initiator = false;
 
-initiateBtn.onclick = (e) => {  
-  initiator = true;  
-  socket1.emit('initiate');  
+const stunServerConfig = {
+  iceServers: [{
+    url: 'turn:13.250.13.83:3478?transport=udp',
+    username: "YzYNCouZM1mhqhmseWk6",
+    credential: "YzYNCouZM1mhqhmseWk6"
+  }]
+};
+
+initiateBtn.onclick = (e) => {
+  initiator = true;
+  socket1.emit('initiate');
+}
+
+stopBtn.onclick = (e) => {
+  socket1.emit('initiate');
+}
+
+socket1.on('initiate', () => {
+  startStream();
+  initiateBtn.style.display = 'none';
+  stopBtn.style.display = 'block';
+})
+
+function startStream () {
+  if (initiator) {
+    // get screen stream
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        mediaSource: "screen",
+        width: { max: '1920' },
+        height: { max: '1080' },
+        frameRate: { max: '10' }
+      }
+    }).then(gotMedia);
+  } else {
+    gotMedia(null);
+  }
+}
+
+function gotMedia (stream) {
+  if (initiator) {
+    var peer = new Peer({
+      initiator,
+      stream,
+      config: stunServerConfig
+    });
+  } else {
+    var peer = new Peer({
+      config: stunServerConfig
+    });
+  }
+
+  peer.on('signal', function (data) {
+    socket1.emit('offer', JSON.stringify(data));
+  });
+
+  socket1.on('offer', (data) => {
+    peer.signal(JSON.parse(data));
+  })
+
+  peer.on('stream', function (stream) {
+    // got remote video stream, now let's show it in a video tag
+    var video = document.querySelector('video');
+    video.srcObject = stream;
+    video.play();
+  })
 }
