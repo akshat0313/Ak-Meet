358bf389c9e25255855acb674fe4251d2176a843
Archisman Karmakar <92569441+ArchismanKarmakar@users.noreply.github.com>
2021-12-24 20:16:46 +0530

    Shortened uuid using short-uuid

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ require('./auth');
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+// const { v4: uuidV4 } = require('uuid')
+const short = require('short-uuid');
+uuidV4 = short.generate
 const { ExpressPeerServer } = require('peer');
 require('dotenv').config()
 var nodemailer = require('nodemailer');
